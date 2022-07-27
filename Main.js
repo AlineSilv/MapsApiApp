c521e5fe65dfba6c0257d055dffc474619998f4d
Aline Silva <alinealv.silv@gmail.com>
2022-07-27 16:00:21 -0300

    commit

--- Main.js
+++ Main.js
@@ -1,13 +1,4 @@
 ﻿
-//set map options
-var myLatLng = { lat: 38.3460, lng: -0.4907 };
-var mapOptions = {
-    center: myLatLng,
-    zoom: 7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-
-};
-
 //create map
 var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
