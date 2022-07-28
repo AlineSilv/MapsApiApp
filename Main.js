151ab72058ec59dfce69f8e8b896ce356e9bed50
Aline Silva <alinealv.silv@gmail.com>
2022-07-28 10:36:47 -0300

    adjustments

--- Main.js
+++ Main.js
@@ -1,5 +1,4 @@
-﻿//javascript.js
-//set map options
+﻿
 var myLatLng = {  lat: 48.85, lng: 2.35 };
 var mapOptions = {
     mapId: "28f08bb4a23eb650",
@@ -10,16 +9,11 @@ var mapOptions = {
 
 };
 
-//create map
+
 var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
-//create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
-
-//create a DirectionsRenderer object which we will use to display the route
 var directionsDisplay = new google.maps.DirectionsRenderer();
 
-//bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
 
 
@@ -39,7 +33,11 @@ function calcRoute() {
 
            
             const output = document.querySelector('#output');
-            output.innerHTML = "<div class='alert-info'>From: " + document.getElementById("from").value + ".<br />To: " + document.getElementById("to").value + ".<br /> Driving distance <i class='fas fa-road'></i> : " + result.routes[0].legs[0].distance.text + ".<br />Duration <i class='fas fa-hourglass-start'></i> : " + result.routes[0].legs[0].duration.text + ".</div>";
+            output.innerHTML = "<div class='alert-info'><strong>From</strong>: " 
+            + document.getElementById("from").value + ".<br /><strong>To</strong>: "
+            + document.getElementById("to").value + ".<br /> <strong>Driving distance</strong> <i class='fas fa-road'></i> : " 
+            + result.routes[0].legs[0].distance.text + ".<br /><strong>Duration</strong> <i class='fas fa-hourglass-start'></i> : " 
+            + result.routes[0].legs[0].duration.text + ".</div>";
 
             
             directionsDisplay.setDirections(result);
